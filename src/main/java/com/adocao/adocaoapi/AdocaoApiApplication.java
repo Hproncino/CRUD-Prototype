4c6487d83caf69652cdf44d255b13dfab9cfc1ce
Hproncino <pimentel2henry@gmail.com>
2026-03-24 21:21:18 -0300

    first commit

---
+++ src/main/java/com/adocao/adocaoapi/AdocaoApiApplication.java
@@ -0,0 +1,12 @@
+package com.adocao.adocaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdocaoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdocaoApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/adocao/adocaoapi/AdocaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adocao.adocaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdocaoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
